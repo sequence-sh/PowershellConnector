e16d28d6ab6a858417d91b9334afe3341567019f
antonyoni <antony@reduc.tech>
2020-12-11 12:34:56 +0000

    Add new tests for different data types

--- Pwsh.Tests/PowerShellRunScriptTests.cs
+++ Pwsh.Tests/PowerShellRunScriptTests.cs
@@ -19,7 +19,7 @@ namespace Reductech.EDR.Connectors.Pwsh.Tests
         {
             get
             {
-                yield return new StepCase("Run PowerShell script",
+                yield return new StepCase("Run PowerShell script that returns a string",
                     new EntityForEach()
                     {
                         EntityStream = new PwshRunScript
@@ -33,6 +33,56 @@ namespace Reductech.EDR.Connectors.Pwsh.Tests
                     },
                     Unit.Default,
                     $"({Entity.PrimitiveKey}: \"hello!\")"
+                );
+                
+                yield return new StepCase("Run PowerShell script that returns nothing but emits a warning",
+                    new EntityForEach()
+                    {
+                        EntityStream = new PwshRunScript
+                        {
+                            Script = Constant(@"Write-Warning 'warning'")
+                        },
+                        Action = new Print<Entity>
+                        {
+                            Value = new GetVariable<Entity> { Variable = VariableName.Entity }
+                        }
+                    },
+                    Unit.Default,
+                    "warning"
+                );
+                
+                yield return new StepCase("Run PowerShell script that returns a stream of ints",
+                    new EntityForEach()
+                    {
+                        EntityStream = new PwshRunScript
+                        {
+                            Script = Constant(@"1..3 | Write-Output")
+                        },
+                        Action = new Print<Entity>
+                        {
+                            Value = new GetVariable<Entity> { Variable = VariableName.Entity }
+                        }
+                    },
+                    Unit.Default,
+                    $"({Entity.PrimitiveKey}: 1)",
+                    $"({Entity.PrimitiveKey}: 2)",
+                    $"({Entity.PrimitiveKey}: 3)"
+                );
+                
+                yield return new StepCase("Run PowerShell script that returns a PSObject",
+                    new EntityForEach()
+                    {
+                        EntityStream = new PwshRunScript
+                        {
+                            Script = Constant(@"[pscustomobject]@{ prop1 = 'one' ; prop2 = 2 } | Write-Output")
+                        },
+                        Action = new Print<Entity>
+                        {
+                            Value = new GetVariable<Entity> { Variable = VariableName.Entity }
+                        }
+                    },
+                    Unit.Default,
+                    "(prop1: \"one\" prop2: 2)"
                 );
             }
         }
